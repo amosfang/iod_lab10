b156d0ec745e49c22898077422bd349abc1c47df
Amos Fang <48528755+amosfang@users.noreply.github.com>
2024-12-20 09:41:49 +0800

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import joblib
 from utils import preprocessor
 
 def run():
-    model = ???
+    model = joblib.load("model.joblib")
 
     st.title("Sentiment Analysis")
     st.text("Basic app to detect the sentiment of text.")
@@ -13,7 +13,7 @@ def run():
     st.text("")
     predicted_sentiment = ""
     if st.button("Predict"):
-        predicted_sentiment = ???
+        predicted_sentiment = model.predict([processed_text])[0] 
         if predicted_sentiment == 1:
             output = 'positive 👍'
         else:
